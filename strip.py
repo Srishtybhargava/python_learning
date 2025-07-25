a62a8e3739e6104268dcd0b4c1764823dc3ad0b1
Srishtybhargava <110772651+Srishtybhargava@users.noreply.github.com>
2025-07-25 11:20:36 +0530

    Chapter2_10

---
+++ strip.py
@@ -0,0 +1,15 @@
+name = "  Srishty  "
+dots = "..........."
+
+#lstrip () method 
+
+print(name+dots)
+print(name.lstrip() + dots) #to remove left space 
+print(name.rstrip() + dots) #to remoce right and left space 
+print(name.strip()+ dots) #to remove both left and right space 
+
+space = ' Srish tea'
+#strip method cannot remoce the space whihc is coming between the variable. 
+#we need to use replace method for that 
+
+print(space.replace(" " , "") + dots)
