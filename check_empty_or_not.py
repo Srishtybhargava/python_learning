026272cf64f265ee005551bfec26e67d1b6f07a7
Srishtybhargava <110772651+Srishtybhargava@users.noreply.github.com>
2025-07-25 17:26:56 +0530

    Day 3

---
+++ check_empty_or_not.py
@@ -0,0 +1,19 @@
+#check empty or not 
+#important 
+
+'''name = "Srishty"
+if name :
+  print("string is not empty")
+else : 
+  print ("string is empty ")'''
+  
+  
+user_input= input ("enter your name")
+  
+if user_input :
+      print (f"string has not empty, it contains name {user_input}")
+     
+else: 
+       print ("you have not typed anything ")
+
+
