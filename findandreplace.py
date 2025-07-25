7c9e1ebdcc6f2f1a0d6e355f8374675bfcb5926b
Srishtybhargava <110772651+Srishtybhargava@users.noreply.github.com>
2025-07-25 11:37:12 +0530

    Chapter 2

--- findandreplace.py
+++ findandreplace.py
@@ -5,7 +5,7 @@ string = "she is beautiful and she is good dancer"
 #print(string.replace(" ", "_"))
 #space is replaced by underscore 
 
-print(string.replace("is", "was", 1))
+print(string.replace("is", "was", 1)) 
 #here one is written to change the only first is in the line 
 
 print(string.find("is"))
