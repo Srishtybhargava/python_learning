ddce5286da0525de21ded686ba55dd60e5df9f34
Srishtybhargava <110772651+Srishtybhargava@users.noreply.github.com>
2025-07-25 11:47:21 +0530

    Chapter 3

---
+++ 3a_ifstatement.py
@@ -0,0 +1,8 @@
+#syntax
+#if condition : 
+  #code : if condition is true then this code will exercute 
+age = int(input ("type your age"))
+if age>= 14:
+ print("You are eligible for the game") #there should be left space mandatory in this #it is use instead of [} 
+
+
