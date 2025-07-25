503dbc157c6fe87393bb6b7bd6359cbcc61eafeb
Srishtybhargava <110772651+Srishtybhargava@users.noreply.github.com>
2025-07-25 17:32:28 +0530

    Day 3

---
+++ while_loop.py
@@ -0,0 +1,12 @@
+#while loop , for loopp 
+
+#print ("hello world ") #10 times 
+
+#while loop 
+
+i = 1  #varaiable assign 
+while i <10 :
+  print (f"hello world {i}") 
+  i = i + 1 ## i = 1 , i =1+1 =2  <10 , i = 10 
+
+
