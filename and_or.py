b49418a69a668f8d3ae2c8dd92610f205effc943
Srishtybhargava <110772651+Srishtybhargava@users.noreply.github.com>
2025-07-25 13:33:36 +0530

    Chapter 3

---
+++ and_or.py
@@ -0,0 +1,19 @@
+#check two conditions at the same time 
+#and , or 
+
+name = "abc"
+age = 19
+#and = both condition should be true to get a true if block 
+if name == "abc" and age==19:
+  print("condition true ")
+else : 
+ print("condition false")
+ 
+ 
+ #or = if one condition is true ;like age == 19 as given , it will you an answer true 
+if name == "abcd" or age==19: 
+    print("condition true ")
+else : 
+   print("condition false")
+ 
+ 
